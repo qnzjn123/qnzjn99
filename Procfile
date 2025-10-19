159aa583dba6299f5d4f50933fb05aa6833c85c6
qnzjn123 <qnzjn123@github.com>
2025-10-19 14:28:00 +0900

    Delete all files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Xmx300m -Xms200m -XX:+UseSerialGC -Dserver.port=$PORT -Djava.security.egd=file:/dev/./urandom -jar target/board-1.0.0.jar
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# JAR 파일 찾기 및 실행
-JAR_FILE=$(find target -name "*.jar" -not -name "*original*" | head -n 1)
-echo "Found JAR file: $JAR_FILE"
-java -Xmx256m -Xms128m -Dserver.port=$PORT -jar $JAR_FILE
